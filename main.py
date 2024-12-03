0eb5a85d519ba499b54ea3fe7d429b6992068caf
richalvarez1216 <richalvarez1216@gmail.com>
2024-12-02 19:15:25 -0500

    asteroid new

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
