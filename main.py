4ee4bdd6b6bd9801d667e28b6244f773991c54df
richalvarez1216 <richalvarez1216@gmail.com>
2024-12-03 19:36:12 -0500

    update asteroid

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pygame
-
+from player import Player
 from constants import *
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create a clock object to manage the frame rate
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Instantiate a Player object in the middle of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -16,8 +23,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        player.update(dt)
+        # fill the screen with black        
         screen.fill((0, 0, 0))
+
+        player.draw(screen)
+        # Refresh the screen
         pygame.display.flip()
+        # Pause the game loop to maintain 60 FPS and get the delta time
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
